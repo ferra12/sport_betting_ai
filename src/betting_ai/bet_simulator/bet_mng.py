9a634898c3386bc395b3a2d0b86bdf9cc6fefdb6
ferra12 <ferrazzo.edoardo@gmail.com>
2025-09-29 12:38:31 +0200

    place_bet aggiunto

--- src/betting_ai/bet_simulator/bet_mng.py
+++ src/betting_ai/bet_simulator/bet_mng.py
@@ -1,9 +1,17 @@
+from ..conn.mongo_conn import MongoDBManager
+from ..models.bet_model import Bet
+from ..utils.config import config
+
+
 class BetManager:
     def __init__(self):
-        pass
+        self.conn = MongoDBManager(
+            config.mongodb_uri, config.mongodb_db_name, config.mongodb_bet_collection
+        )
 
-    def place_bet(self):
-        pass
+    def place_bet(self, bet: Bet):
+        """Carica una nuova scommessa sul database."""
+        self.conn.insert_one(bet)
 
     def cancel_bet(self):
         pass
